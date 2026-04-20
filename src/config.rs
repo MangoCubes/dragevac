40b34796dc1bdea9cb1f88a62656e45edff96dd3
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-04-21 00:01:06 +0900

    Use default config if not exists

--- src/config.rs
+++ src/config.rs
@@ -2,29 +2,49 @@ use serde::Deserialize;
 use std::fs;
 use std::path::PathBuf;
 
-#[derive(Deserialize, Debug, Default)]
+#[derive(Deserialize, Debug)]
 pub struct Config {
-    pub css: Option<String>,
+    pub css: String,
 }
 
-fn get_config_dir() -> PathBuf {
-    PathBuf::from(match env::var("XDG_CONFIG_HOME") {
-        Ok(home) => home,
-        Err(e) => {
-            println!(
-                "Failed go get XDG_CONFIG_HOME ({}). Falling back to HOME.",
-                e.to_string()
-            );
-            env::var("XDG_CONFIG_HOME").expect("You don't have HOME set too??")
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            css: include_str!("./default.css").to_string(),
         }
-    })
-    .join(".config")
+    }
+}
+
+fn get_config_dir() -> Option<PathBuf> {
+    Some(
+        PathBuf::from(match env::var("XDG_CONFIG_HOME") {
+            Ok(home) => home,
+            Err(e) => {
+                println!(
+                    "Failed go get XDG_CONFIG_HOME ({}). Falling back to HOME.",
+                    e.to_string()
+                );
+                if let Ok(p) = env::var("XDG_CONFIG_HOME") {
+                    p
+                } else {
+                    println!(
+                        "Failed go get HOME ({}). Using default config.",
+                        e.to_string()
+                    );
+                    return None;
+                }
+            }
+        })
+        .join(".config"),
+    )
 }
 
 pub fn load_config() -> Config {
-    let config_path = get_config_dir().join("dragbox/config.json");
+    let Some(config_path) = get_config_dir() else {
+        return Config::default();
+    };
 
-    fs::read_to_string(config_path)
+    fs::read_to_string(config_path.join("dragbox/config.json"))
         .ok()
         .and_then(|c| serde_json::from_str(&c).ok())
         .unwrap_or_default()
