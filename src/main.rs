2447fc4e8963885a03dfc47c4e99ed0e65c92bd6
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-04-24 02:39:08 +0900

    Add new modes

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,19 @@ enum Command {
     /// Writes default config. If config already exists, this will fill out missing fields, delete
     /// invalid fields, and write the result back.
     Config,
+    /// Default execution method. The entry list is completely empty, and can be filled by dropping
+    /// items into it. The list is not saved when the program exits.
+    NoSave,
+    // /// The entry list is saved into a specific directory in /tmp. The entry list state file is
+    // /// updated whenever user adds or removes entries from them. This allows user to have a
+    // /// persistent list that automatically resets when the computer reboots.
+    // Temporary,
+    // /// Stores the entries in a specified location for complete permanence. If the user does not
+    // /// specify the location to store the entries, $XDG_DATA_HOME/dragbox/state.json will be used.
+    // Persistent,
+    // /// User cannot drag items into the list. User must specify the state file and/or a preload
+    // /// directory from which the list of entries will be pre-filled.
+    // ReadOnly,
 }
 
 fn main() {
@@ -47,7 +60,7 @@ fn main() {
         Some(Command::Config) => {
             config::write_config(config_path.as_deref());
         }
-        None => {
+        None | Some(Command::NoSave) => {
             let app = Application::builder()
                 .application_id("ch.skew.dragbox")
                 .build();
