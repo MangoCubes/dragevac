0817a69275abeb4a87c2b9700a85569657124863
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-06-01 01:08:21 +0900

    Using rust overlay

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs =
@@ -11,6 +12,7 @@
       self,
       nixpkgs,
       flake-utils,
+      rust-overlay,
     }:
     {
       homeManager = {
@@ -20,21 +22,27 @@
     // flake-utils.lib.eachDefaultSystem (
       system:
       let
+        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system;
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-src"
+            "rust-analyzer"
+          ];
         };
         # Things needed at build time only, something that doesn't have to exist on the system when running the compiled program
         # `pkg-config` is a helper tool used when compiling applications and libraries, so it is only needed when the program is being compiled
         nativeBuildInputs = [ pkgs.pkg-config ];
         # Things needed at runtime, something that must be installed on the computer in order to run the program
-        buildInputs = (
-          with pkgs;
-          [
+        buildInputs =
+          (with pkgs; [
             gtk4
             gtk4-layer-shell
             glib
-          ]
-        );
+          ])
+          ++ [ rustToolchain ];
       in
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
@@ -49,7 +57,6 @@
           packages = (
             with pkgs;
             [
-              rustup
 
               # This package is needed to ensure subsequent shells looks pretty
               bashInteractive
